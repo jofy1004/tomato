d429634b418eade925b2319b17ee3d42687d5d2e
fzhou <jofy1004@gmail.com>
2019-02-15 14:05:24 +0800

    配置tomcat启动；

--- src/main/java/com/tomato/TomatoApplication.java
+++ src/main/java/com/tomato/TomatoApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -14,10 +16,14 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @EnableScheduling
 @EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @PropertySources({
-        @PropertySource("classpath:config/jdbc.${spring.profiles.active}.properties"),
+        @PropertySource("classpath:config/jdbc.dev.properties"),
         @PropertySource("classpath:application.properties")
 })
-public class TomatoApplication {
+public class TomatoApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(TomatoApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(TomatoApplication.class, args);
